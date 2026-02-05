5899b524392351d977280ad8d07da87a575cad79
sakincse21 <saleheen.sakin@gmail.com>
2026-02-05 09:48:39 +0600

    test branch test

---
+++ src/test/java/com/example/webapp/controller/StudentControllerTest.java
@@ -0,0 +1,20 @@
+package com.example.webapp.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void getStudents() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void storeStudent() {
+    }
+}
